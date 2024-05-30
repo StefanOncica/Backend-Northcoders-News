824bc1a3b01a06a4578023115dc49df9bd7f0eda
stefan <lioncatnewyork@gmail.com>
2024-05-30 10:01:04 +0100

    Solves Task 6 - adds GET/api/articles/articleId/comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {getTopics, getEndpoints, getArticleById, getArticles} = require('./controllers/topics.controllers')
+const {getTopics, getEndpoints, getArticleById, getArticles, getAllCommentsByArticleId} = require('./controllers/topics.controllers')
 
 app.use(express.json())
 
@@ -12,8 +12,14 @@ app.get('/api/articles/:id', getArticleById )
 
 app.get('/api/articles', getArticles)
 
+app.get('/api/articles/:article_id/comments', getAllCommentsByArticleId)
+
 app.use((req, res, next) => {
     res.status(404).send({msg: "Endpoint doesn't exist."})
+})
+
+app.use((err, req, res, next) => {
+    res.status(400).send({msg: 'Bad request.'})
 })
 
 module.exports = app
